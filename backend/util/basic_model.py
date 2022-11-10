24958821bc20e340769053b5a2caf38137838ea3
kadenbking <kadenbking@gmail.com>
2022-11-09 20:13:07 -0600

    added basic model

--- backend/util/basic_model.py
+++ backend/util/basic_model.py
@@ -4,10 +4,20 @@ from sklearn.model_selection import train_test_split
 from leagues import NFL
 
 
-# FIELDS_TO_DROP = ['away_runs', 'home_runs', 'date', 'location',
-#                   'losing_abbr', 'losing_name', 'winner', 'winning_abbr',
-#                   'winning_name']
+FIELDS_TO_DROP = ['points_allowed', 'points_scored', 'location', 'overtime', 'time_of_possession',
+                  'opponent_abbr', 'opponent_name', 'result', 'type']
 
-# X = dataset.drop(FIELDS_TO_DROP, 1).dropna().drop_duplicates()
-
-print(NFL().dataset)
+dataset = NFL().dataset
+dataset = dataset.iloc[: , 5:]
+X = dataset.drop(columns=FIELDS_TO_DROP)
+y = dataset[['points_scored', 'points_allowed']].values
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+parameters = {'bootstrap': False,
+              'min_samples_leaf': 3,
+              'n_estimators': 50,
+              'min_samples_split': 10,
+              'max_features': 'sqrt',
+              'max_depth': 6}
+model = RandomForestRegressor(**parameters)
+model.fit(X_train, y_train)
+print(model.predict(X_test).astype(int), y_test)
